ab7d5cffa236dacd6717c95b7919108721391e6d
Navaneeth V <navaneethv.21it@kongu.edu>
2023-10-24 15:01:05 +0530

    eyesee opticals

---
+++ src/pages/payment/Payment.js
@@ -0,0 +1,25 @@
+// Payment.js
+import React from "react";
+import { Link } from "react-router-dom";
+import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
+import imge from '../pay.jpg'; // Update the image path if needed
+
+const Payment = () => {
+  return (
+    <div className="max-w-container mx-auto px-4">
+      <Breadcrumbs title="Payment gateway" />
+      <img src={imge} alt="anva" width={250} />
+      
+      <div className="pb-10">
+        <p>Payment gateway only applicable for Production build.</p>
+        <Link to="/">
+          <button className="w-52 h-10 bg-primeColor text-white text-lg mt-4 hover:bg-black duration-300">
+            Explore More
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
